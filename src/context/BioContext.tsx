f788acb5a0ec5b623acdef69706ad599617db2c8
Sary J <sary.jalkh@gmail.com>
2025-01-08 17:29:47 -0500

    context API

---
+++ src/context/BioContext.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+interface BioContextTypes = {
+    output: {data:  {bio: string}[]},
+    loading: boolean,
+    setOutput: React.Dispatch<React.SetStateAction<{data: {bio: {bio: string}[]}}>>
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const BioContext = createContext<BioContextTypes>({
+    output: {data: [] },
+    loading: false,
+    setOutput: () => {},
+    setLoading: () => {},
+});
+
+export const BioProvider = ({children}: {children: React.ReactNode}) {
+    const [output, setOutput] = useState<{data:  {bio: string}[]}>({data: []})
+    const [loading, setLoading] = useState(false);
+
+
+    return <BioContext.Provider values={{output, setOutput, setLoading, loading}} >
+    {children}
+    </BioContext.Provider>
+}
