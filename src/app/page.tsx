591f491f3c8c59b8a233320b49330eb8b8fd7c89
Sary J <sary.jalkh@gmail.com>
2025-01-09 16:54:28 -0500

    working on responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { BioProvider } from "@/context/BioContext";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center pb-24 pt-16 md:px-8 xl:px-24">
+    <main className="flex min-h-screen flex-col items-center pb-24 pt-16 md:px-8 xl:px-24 px-6">
       <div className="z-10 bg-white mb-10 mx-auto">
         <div className="group rounded-full border border-black/10 transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800 group">
           <Link href="https://github.com" target="_blank" className="mb-4">
@@ -21,15 +21,15 @@ export default function Home() {
         </div>
       </div>
       <div className="text-center z-10">
-        <h1 className="font-extrabold text-7xl lg:w-[90%] mx-auto mb-8">
+        <h1 className="font-extrabold md:text-7xl text-5xl lg:w-[90%] mx-auto mb-8">
           Generate your twitter bio
         </h1>
-        <p className="text-lg text-gray-500 mx-auto mb-8">
+        <p className="text-base md:text-lg text-gray-500 mx-auto mb-8">
           Answer a few questions, and we&apos;ll craft a unique bio that
           perfectly reflects your personality.
         </p>
       </div>
-      <div className="z-10 flex gap-10 justify-between w-full">
+      <div className="z-10 flex flex-col md:flex-row gap-10 justify-between w-full">
         <BioProvider>
           <div className="flex-1">
             <UserInput />
